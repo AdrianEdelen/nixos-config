e4df3b8796a6b7882c63e8377d78319fe3521a4e
Tofu <adrianedelen@gmail.com>
2024-08-01 11:06:25 -0400

    updated hardware config for vm to use labels.

--- configurations/vm/hardware-configuration.nix
+++ configurations/vm/hardware-configuration.nix
@@ -14,17 +14,17 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/399b145c-3496-464b-bb41-7fa60c43832f";
+    { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3CD6-44F9";
+    { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
