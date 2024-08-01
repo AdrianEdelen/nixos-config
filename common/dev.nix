99f990a74feeb0cce4fe84d729c212f42c07edb9
Tofu <adrianedelen@gmail.com>
2024-08-01 13:26:01 -0400

    add git config inof to dev.nix

--- common/dev.nix
+++ common/dev.nix
@@ -4,4 +4,21 @@
   environment.systemPackages = with pkgs; [
     git
   ];
+
+  
+  environment.etc."gitconfig".text = ''
+    [user]
+        name = "adrian"
+        email = "adrian@edelen.haus"
+    [core]
+        editor = "vim"
+    [color]
+        ui = auto
+    [alias]
+        co = checkout
+        br = branch
+        ci = commit
+        st = status
+  '';
+
 }
