656ddf7c64735b021af6e8e3b2826fec2e612c4d
Tofu <adrianedelen@gmail.com>
2024-08-01 10:43:42 -0400

    fixed missing semicolon in users.nix

--- common/users.nix
+++ common/users.nix
@@ -4,7 +4,7 @@
 {
     users.users.adrian = {
         isNormalUser = true;
-        password = "$y$j9T$JCcuaa0bALWxON4dBQExy0$A5PHtEVd8n5s1Hc7LYVx6IPx7WIP9ZhBeRrGbFZcFf3"
-        extraGroups = [ "wheel" ]
+        password = "$y$j9T$JCcuaa0bALWxON4dBQExy0$A5PHtEVd8n5s1Hc7LYVx6IPx7WIP9ZhBeRrGbFZcFf3";
+        extraGroups = [ "wheel" ];
     }
 }
