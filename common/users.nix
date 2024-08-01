d243d8dab629a90776b5f9f3bd8ad9e0d2d7ddd5
Tofu <adrianedelen@gmail.com>
2024-08-01 11:44:26 -0400

    changed user test pass to hashedPassword

--- common/users.nix
+++ common/users.nix
@@ -11,7 +11,7 @@
 
         adrian = {
             isNormalUser = true;
-            password = "$6$LHbxg9QDCeflSqEx$VZuK00PzAFEQaI2uszoqvz8hlonxnU4NZ/tGTK0eB1wCi2hetSYnRymO65ebTFx2pH9MtbhVlrjbxX3AinAu/.";
+            hashedPassword = "$6$LHbxg9QDCeflSqEx$VZuK00PzAFEQaI2uszoqvz8hlonxnU4NZ/tGTK0eB1wCi2hetSYnRymO65ebTFx2pH9MtbhVlrjbxX3AinAu/.";
             extraGroups = [ "wheel" ];
         };
     };
