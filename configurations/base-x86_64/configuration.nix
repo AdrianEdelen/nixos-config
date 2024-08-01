f7737bf2894aee127ac732c583ea20fbfe0a04a1
Tofu <adrianedelen@gmail.com>
2024-08-01 13:09:11 -0400

    commented out locale stuff for now

--- configurations/base-x86_64/configuration.nix
+++ configurations/base-x86_64/configuration.nix
@@ -20,12 +20,12 @@
   networking.networkmanager.enable = true;
   time.timeZone = "America/New_York";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
 
   system.stateVersion = "24.05"; 
 
