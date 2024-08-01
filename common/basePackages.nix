290ecfcc3f19ebf9f3e464be36ae8c65c666bec1
Tofu <adrianedelen@gmail.com>
2024-08-01 13:55:33 -0400

    enable system wide gpg-agent

--- common/basePackages.nix
+++ common/basePackages.nix
@@ -9,5 +9,14 @@
     tree
     sops
     gnupg
+    pinentry
   ];
+
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 600;
+    maxCacheTtl = 7200;
+    pinentryFlavor = "gtk2";
+  };
 }
