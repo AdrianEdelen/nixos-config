9291dc4737b24619a465878450c6b73f07c2de74
Tofu <adrianedelen@gmail.com>
2024-08-01 11:37:30 -0400

    fixed semicolon

--- common/users.nix
+++ common/users.nix
@@ -14,5 +14,5 @@
             password = "$6$LHbxg9QDCeflSqEx$VZuK00PzAFEQaI2uszoqvz8hlonxnU4NZ/tGTK0eB1wCi2hetSYnRymO65ebTFx2pH9MtbhVlrjbxX3AinAu/.";
             extraGroups = [ "wheel" ];
         };
-    }
+    };
 }
