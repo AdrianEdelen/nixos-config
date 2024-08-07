4f040fbab38c3a629315dc6502df3fe9a2ac6d5e
Tofu <adrian@edelen.haus>
2024-08-07 13:43:54 -0400

    TYPO TYPO TYPO

--- common/users.nix
+++ common/users.nix
@@ -13,7 +13,7 @@
             isNormalUser = true;
             hashedPassword = "$6$LHbxg9QDCeflSqEx$VZuK00PzAFEQaI2uszoqvz8hlonxnU4NZ/tGTK0eB1wCi2hetSYnRymO65ebTFx2pH9MtbhVlrjbxX3AinAu/.";
             extraGroups = [ "wheel" ];
-            openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyWYKha5v+ubkEDWg9ak4iop/5ghspe8vV8poRLfj81 adrian@edelen.haus" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvAS6S17aYbEVztdQxNkfS/ziTg6JVx4uBLzaccmvfm JuiceSSH ]
+            openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyWYKha5v+ubkEDWg9ak4iop/5ghspe8vV8poRLfj81 adrian@edelen.haus" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvAS6S17aYbEVztdQxNkfS/ziTg6JVx4uBLzaccmvfm JuiceSSH ];
         };
     };
 }
