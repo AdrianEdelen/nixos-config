5800ccbd4b474f5861554ca31a2f1cbd9f587aa4
Adrian Edelen <adrian@edelen.haus>
2025-03-19 17:05:25 -0400

    remove server flake

--- configurations/base-x86_64/vm/vm-tty/configuration.nix
+++ configurations/base-x86_64/vm/vm-tty/configuration.nix
@@ -13,6 +13,10 @@
     passwordAuthentication = false;
   };
 
+  users.users.admin {
+    isNormalUser = true
+  }
+
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
