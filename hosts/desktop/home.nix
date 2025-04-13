4059d333e0997e1f47c62b38ef7f10a78ab04977
Adrian <adrian@edelen.haus>
2025-04-13 17:12:48 -0400

    added config files

---
+++ hosts/desktop/home.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "adrian";
+  home.homeDirectory = "/home/adrian";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    bat
+    eza
+    starship
+  ];
+
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+
+  home.stateVersion = "24.05";
+}
