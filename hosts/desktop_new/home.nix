2940ee943013ff5e476eb2d910a1fbe968abbd78
Adrian E <adrian@edelen.haus>
2025-07-22 15:09:44 -0400

    add current workstation public key

--- hosts/desktop_new/home.nix
+++ hosts/desktop_new/home.nix
@@ -1,4 +1,11 @@
 { config, pkgs, ... }:
 {
   home.stateVersion = "25.05";
+
+  programs.ssh = {
+    enable = true;
+    authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGAx3hpEtbFlli31t70VqJ/7enknFbtlIR3fpa5vTXBM adrian@box" # <--- REPLACE THIS with your actual public key
+    ];
+  };
 }
