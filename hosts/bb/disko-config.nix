d16ab6e00f3d6bc70aeff43955f68ee1b42cf9cf
Adrian E <adrian@edelen.haus>
2025-07-29 12:17:21 -0400

    disko config update for bb host

---
+++ hosts/bb/disko-config.nix
@@ -0,0 +1,32 @@
+{
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
